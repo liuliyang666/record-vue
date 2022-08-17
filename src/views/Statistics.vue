0a948650d0ac80165d9745a26d44c8f81afe7666
lilyyang <1592218619@qq.com>
2022-08-17 21:54:22 +0800

    创建三个页面

---
+++ src/views/Statistics.vue
@@ -0,0 +1,16 @@
+import Statistics from '@/views/Statistics.vue';
+<template>
+    <div>
+        Statistics.vue
+    </div>
+</template>
+
+<script lang="ts">
+    export default ({
+        name: 'Statistics'
+    })
+</script>
+
+<style lang='scss' scoped>
+
+</style>
