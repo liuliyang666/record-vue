2a3adef72d31a8c0b7a480de83a5366c9237661e
lilyyang <1592218619@qq.com>
2022-08-18 23:06:04 +0800

    Money 布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">Money.vue</div>
+
     <Nav />
   </div>
 </template>
@@ -11,4 +12,16 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  flex-grow: 1;
+  overflow: auto;
+}
+</style>
