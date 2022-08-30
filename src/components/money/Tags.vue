f4c81445b21197401e1a5db41973c2c6f76fe8ff
lilyyang <1592218619@qq.com>
2022-08-31 00:14:40 +0800

    Label.vue使用Vuex

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li
@@ -17,8 +17,10 @@
 </template>
 
 <script lang="ts">
+import TagHelper from "@/mixins/TagHelper";
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+import { mixins } from "vue-class-component";
 
 @Component({
   computed: {
@@ -27,11 +29,13 @@ import { Component, Prop } from "vue-property-decorator";
     },
   },
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
+
   created() {
     this.$store.commit("fetchTags");
   }
+
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -40,13 +44,6 @@ export default class Tags extends Vue {
       this.selectedTags.push(tag);
     }
     this.$emit("update:value", this.selectedTags);
-  }
-  create() {
-    const name = window.prompt("请输入标签名");
-    if (!name) {
-      return window.alert("标签名不能为空");
-    }
-    this.$store.commit("createTag", name);
   }
 }
 </script>
