9c33a8c69e2a6a6446e56d715ca5288ef3e253f6
lilyyang <1592218619@qq.com>
2022-09-22 14:43:42 +0800

    自动更新options

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,6 +5,10 @@
       :data-source="recordTypeList"
       :value.sync="type"
     />
+    <div class="chart-wrapper" ref="chartWrapper">
+      <Chart class="chart" :options="chartOptions" />
+    </div>
+
     <ol v-if="groupedList.length > 0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">
@@ -29,12 +33,20 @@ import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 import dayjs from "dayjs";
 import clone from "@/lib/clone";
+import Chart from "@/components/Chart.vue";
+import _ from "lodash";
+import day from "dayjs";
+
 @Component({
-  components: { Tabs },
+  components: { Tabs, Chart },
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
     return tags.length === 0 ? "无" : tags.map((t) => t.name).join("，");
+  }
+  mounted() {
+    const div = this.$refs.chartWrapper as HTMLDivElement;
+    div.scrollLeft = div.scrollWidth;
   }
   beautify(string: string) {
     const day = dayjs(string);
@@ -52,10 +64,82 @@ export default class Statistics extends Vue {
       return day.format("YYYY年M月D日");
     }
   }
+  get keyValueList() {
+    const today = new Date();
+    const array = [];
+    console.log(this.groupedList);
+    for (let i = 0; i <= 29; i++) {
+      // this.recordList = [{date:7.3, value:100}, {date:7.2, value:200}]
+      const dateString = day(today).subtract(i, "day").format("YYYY-MM-DD");
+      const found = _.find(this.groupedList, {
+        title: dateString,
+      });
+      array.push({
+        key: dateString,
+        value: found ? found.total : 0,
+      });
+    }
+    array.sort((a, b) => {
+      if (a.key > b.key) {
+        return 1;
+      } else if (a.key === b.key) {
+        return 0;
+      } else {
+        return -1;
+      }
+    });
+    console.log("array");
+    console.log(array);
+    return array;
+  }
+  get chartOptions() {
+    const keys = this.keyValueList.map((item) => item.key);
+    const values = this.keyValueList.map((item) => item.value);
+    console.log("values");
+    console.log(values);
+    return {
+      grid: {
+        left: 0,
+        right: 0,
+      },
+      xAxis: {
+        type: "category",
+        data: keys,
+        axisTick: { alignWithLabel: true },
+        axisLine: { lineStyle: { color: "#666" } },
+        axisLabel: {
+          formatter: function (value: string, index: number) {
+            return value.substr(5);
+          },
+        },
+      },
+      yAxis: {
+        type: "value",
+        show: false,
+      },
+      series: [
+        {
+          symbol: "circle",
+          symbolSize: 12,
+          itemStyle: { borderWidth: 1, color: "#666", borderColor: "#666" },
+          // lineStyle: {width: 10},
+          data: values,
+          type: "line",
+        },
+      ],
+      tooltip: {
+        show: true,
+        triggerOn: "click",
+        position: "top",
+        formatter: "{c}",
+      },
+    };
+  }
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
   get groupedList() {
+    console.log("grouped list 被读取了");
     const { recordList } = this;
     const newList = clone(recordList)
       .filter((r) => r.type === this.type)
@@ -86,8 +170,6 @@ export default class Statistics extends Vue {
     }
     result.map((group) => {
       group.total = group.items.reduce((sum, item) => {
-        console.log(sum);
-        console.log(item);
         return sum + item.amount;
       }, 0);
     });
@@ -100,8 +182,11 @@ export default class Statistics extends Vue {
   recordTypeList = recordTypeList;
 }
 </script>
-
 <style scoped lang="scss">
+.echarts {
+  max-width: 100%;
+  height: 400px;
+}
 .noResult {
   padding: 16px;
   text-align: center;
@@ -138,5 +223,14 @@ export default class Statistics extends Vue {
   margin-right: auto;
   margin-left: 16px;
   color: #999;
+}
+.chart {
+  width: 430%;
+  &-wrapper {
+    overflow: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
 }
 </style>
